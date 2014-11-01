751ab7e2b3356b48d1fe8e1f9b8001f46efea9ad
P3trovichUA <mykola.zarichnyuk@gmail.com>
2014-11-01 16:33:48 +0200

    Реалізовано прилипання футера на flexbox

--- js/common.js
+++ js/common.js
@@ -2,31 +2,40 @@
 if(!window.console) window.console = {};
 if(!window.console.log) window.console.log = function() {};
 
-$(function() {
-
 // footer
 //-----------------------------------------------------------------------------
-  var
-    $pageWrapper = $('#page-wrapper'),
-    footer  = {
-      $element: $('#footer'),
-      height: null,
-      place: function() {
-        var self = this;
-        self.height = self.$element.outerHeight();
-        $pageWrapper.css({paddingBottom: self.height});
-        self.$element.css({marginTop: -self.height});
-      }
-    };
+if(!Modernizr.flexbox || ieDetector.ieVersion) {
+  (function() {
+    var
+      $pageWrapper = $('#page-wrapper'),
+      footer  = {
+        $element: $('#footer'),
+        height: null,
+        $preFooter: $('<div/>').attr({id: 'pre-footer'}),
+        place: function() {
+          var
+            self = this;
+          self.height = self.$element.outerHeight();
+          if(!$('#pre-footer').length) {
+            $pageWrapper.append(self.$preFooter);
+          }
+          self.$preFooter.height(self.height);
+          self.$element.css({top: -self.height});
+        }
+      };
 
-  $(window).on({
-    load: function() {
-      footer.place();
-    },
-    resize: function() {
-      footer.place();
-    }
-  });
+    $(window).on({
+      load: function () {
+        footer.place();
+      },
+      resize: function () {
+        footer.place();
+      }
+    });
+  })();
+}
+
+$(function() {
 
 // placeholder
 //-----------------------------------------------------------------------------
